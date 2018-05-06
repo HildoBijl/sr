eeaea97cee8e4854181e4ccc131179f0a3d76051
HildoBijl <hildobijl@gmail.com>
2018-05-06 22:43:32 +0200

    Add option to delete news items and experiences

--- src/ui/components/NewsOverview/NewsOverview.js
+++ src/ui/components/NewsOverview/NewsOverview.js
@@ -18,7 +18,7 @@ class NewsOverview extends Component {
 
 		// Load the news, if necessary.
 		if (!this.state.newsLoaded) {
-			firebase.database().ref(`public/${this.getType()}`).orderByChild('title').limitToLast(this.getMaxItems()).once('value').then((snapshot) => {
+			firebase.database().ref(`public/${this.getType()}`).orderByChild('date').limitToLast(this.getMaxItems()).once('value').then((snapshot) => {
 				// Read the news and turn it into an array.
 				const news = snapshot.val()
 				this.news = []
@@ -45,6 +45,24 @@ class NewsOverview extends Component {
 		return this.props.type || 'news'
 	}
 
+	deleteNews(item) {
+		// Check if this is what the user wanted.
+		if (!window.confirm(`Weet je zeker dat je het bericht "${item.title}" wilt verwijderen?`))
+			return
+			
+		// Delete it from the storage of this page.
+		this.news.splice(this.news.indexOf(item), 1)
+		this.forceUpdate()
+
+		// Delete the image from the storage and the record from the database.
+		let imgData = item.picture.match(/images%2F(.*)\?/)
+		if (imgData) {
+			const imgUrl = imgData[1]
+			firebase.storage().ref(`images`).child(imgUrl).delete()
+		}
+		firebase.database().ref(`public/${this.getType()}`).child(item.id).remove()
+	}
+
 	render() {
 		if (!this.news || this.news.length === 0)
 			return <div className="newsOverview"></div>
@@ -62,7 +80,7 @@ class NewsOverview extends Component {
 			<div key={item.id} className="item">
 				{item.picture ? <img src={item.picture} alt={item.title} /> : ''}
 				<h3 className="title">{item.title}</h3>
-				{isAdmin(this.props.user) ? <p className="edit"><a onClick={() => this.props.editNews(item, this.getType())}>Wijzig {this.getType() === 'news' ? 'dit nieuwsbericht' : 'deze ervaring'}</a></p> : ''}
+				{isAdmin(this.props.user) ? <p className="edit"><a onClick={() => this.props.editNews(item, this.getType())}>Wijzig {this.getType() === 'news' ? 'dit nieuwsbericht' : 'deze ervaring'}</a> - <a onClick={() => this.deleteNews(item)}>Verwijder {this.getType() === 'news' ? 'dit nieuwsbericht' : 'deze ervaring'}</a></p> : ''}
 				{paragraphs.map((paragraph, i) => <p key={i}>{paragraph}</p>)}
 			</div>
 		)
@@ -74,8 +92,8 @@ const stateMap = (state) => ({
 })
 const actionMap = (dispatch) => ({
 	editNews: (item, type) => {
-		dispatch(formCacheActions.storeFormData(item, (type === 'news' ? 'EDITNEWS' : 'EDITEXPERIENCE'))) // TODO: FIX THIS! (Not sure how yet.)
-		dispatch({ type: (type === 'news' ? 'EDITNEWS' : 'EDITEXPERIENCE') })
+		dispatch(formCacheActions.storeFormData(item, (type === 'news' ? 'EDITNEWS' : 'EDITEXPERIENCE'))) // Put the data in the form cache.
+		dispatch({ type: (type === 'news' ? 'EDITNEWS' : 'EDITEXPERIENCE') }) // Go to the right page.
 	},
 })
 export default connect(stateMap, actionMap)(NewsOverview)
